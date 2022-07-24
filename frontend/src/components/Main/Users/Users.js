b5c69e94dde58de118116a807121840260dd95c4
Aleksandr <medovnikaleksander@yandex.ua>
2022-07-24 17:31:44 +0300

    added format

--- frontend/src/components/Main/Users/Users.js
+++ frontend/src/components/Main/Users/Users.js
@@ -2,24 +2,24 @@ import React, { useEffect } from "react";
 
 
 const Users = (props) => {
-     useEffect(() => {
-        if (props.users.length === 0){
+    useEffect(() => {
+        if (props.users.length === 0) {
             fetch("api/user")
                 .then(response => response.json())
                 .then(users => props.getUsers(users))
         }
-     }, []);
+    }, []);
 
-     const users = props.users.map(user => {
-            return (
-                <li key={user.id} id={user.id}>
-                    <h3>{user.name}</h3>
-                    <p>Возраст: {user.age}</p>
-                    <p>Страна: {user.country}</p>
-                    <p>Компания: {user.company}</p>
-                </li>
-            )
-        })
+    const users = props.users.map(user => {
+        return (
+            <li key={user.id} id={user.id}>
+                <h3>{user.name}</h3>
+                <p>Возраст: {user.age}</p>
+                <p>Страна: {user.country}</p>
+                <p>Компания: {user.company}</p>
+            </li>
+        )
+    })
 
     return (
         <div>
