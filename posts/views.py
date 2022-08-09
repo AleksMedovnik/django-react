eb77eedfc496a8a5810f06b564ae8a6448748ccf
Aleksandr <medovnikaleksander@yandex.ua>
2022-08-09 10:37:58 +0300

    added userProfile

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from .models import Post
 from .serializers import PostSerializer
 from rest_framework import viewsets
 
+
 class PostListCreate(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
